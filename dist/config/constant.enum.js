1b6f236d6942178fa107c19bf556464d4f670c36
suriya.t <suriya.t@payoda.com>
2021-05-29 18:46:39 +0530

    dist deleted

--- dist/config/constant.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DBConnection = void 0;
-var DBConnection;
-(function (DBConnection) {
-    DBConnection["dbConnection"] = "mongodb+srv://Alsiraj:Suriya95!@cluster0.pbkvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-})(DBConnection = exports.DBConnection || (exports.DBConnection = {}));
-//# sourceMappingURL=constant.enum.js.map
